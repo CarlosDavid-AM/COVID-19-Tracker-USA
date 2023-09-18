9a959fee803bd372767a2e22a549ad68ebc27674
Carlos David <apolayamendozacarlos@gmail.com>
2023-09-18 18:43:10 -0500

    Se hace una lijera modificacion a la tablas y se prepara para la siguiente face

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ function App() {
 
   return (
     <div>
-        <div class="bg-blue-500 p-4 text-white">
-            <h1 class="text-2xl font-bold">
+        <div className="bg-blue-500 p-4 text-white">
+            <h1 className="text-2xl font-bold">
                 ¡Bienvenido a COVID-19 Tracker USA!
             </h1>
-            <p class="mt-2">
+            <p className="mt-2">
                 Te damos la bienvenida a esta herramienta que te proporciona información sobre la situación pasada del COVID-19 en Estados Unidos 
                 hasta el 7 de marzo de 2021, basada en datos confiables. Aunque no se actualicen datos recientes, 
                 este proyecto sigue siendo una fuente valiosa de referencia y un ejemplo de mi trabajo en el desarrollo web. 
@@ -31,32 +31,38 @@ function App() {
             </p>
         </div>
 
-        <div>
-          {
-            covid.map(({date, dateChecked, hash}) => (
-              <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <table class="min-w-full">
-                  <thead class="bg-gray-200">
-                    <tr>
-                      <th class="py-2 px-4 text-left">Fecha</th>
-                      <th class="py-2 px-4">Estados</th>
-                      <th class="py-2 px-4">Positivos</th>
-                      <th class="py-2 px-4">Negativos</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td class="py-2 px-4">20210307</td>
-                      <td class="py-2 px-4">56</td>
-                      <td class="py-2 px-4">28,756,489</td>
-                      <td class="py-2 px-4">74,582,825</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-            ))
-          }
+        <div className="flex">
+          <div className="w-1/2  border border-black">
+            <div className="pl-20">
+              fgg
+            </div>
+          </div>
+          <div className="w-full">
+            {
+              covid.map(({states, dateChecked, positive, negative, hash}) => (
+                <div key={hash} className="bg-white shadow-md rounded-lg overflow-hidden">
+                  <table className="min-w-full">
+                    <thead className="bg-gray-200">
+                      <tr>
+                        <th className="py-2 px-4 text-left">Fecha</th>
+                        <th className="py-2 px-4">Estados</th>
+                        <th className="py-2 px-4">Positivos</th>
+                        <th className="py-2 px-4">Negativos</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td  className="py-2 px-4 align-middle text-left">{dateChecked}</td>
+                        <td  className="py-2 px-4 align-middle text-center">{states}</td>
+                        <td  className="py-2 px-4 align-middle text-center">{positive}</td>
+                        <td  className="py-2 px-4 align-middle text-center">{negative}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              ))
+            }
+          </div>
         </div>
   </div>
   )
